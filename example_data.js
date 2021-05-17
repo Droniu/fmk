41ce21302dea1c57b4cb8f45b7eec9b2714fc667
Droniu <dron.official@yahoo.com>
2021-05-17 07:44:56 +0200

    example data

---
+++ example_data.js
@@ -0,0 +1,44 @@
+data = `
+[
+    {
+        "name": "Aliesha Estrada",
+        "photo": "fmkphotos/fmk1.jpg"
+    },
+    {
+        "name": "Maria Nguyen",
+        "photo": "fmkphotos/fmk2.jpg"
+    },
+    {
+        "name": "Kathleen Hunter",
+        "photo": "fmkphotos/fmk3.jpg"
+    },
+    {
+        "name": "Lola Figueroa",
+        "photo": "fmkphotos/fmk4.jpg"
+    },
+    {
+        "name": "Roberto Guzman",
+        "photo": "fmkphotos/fmk5.jpg"
+    },
+    {
+        "name": "Arnold Vasquez",
+        "photo": "fmkphotos/fmk6.jpg"
+    },
+    {
+        "name": "Cindy Schwartz",
+        "photo": "fmkphotos/fmk7.jpg"
+    },
+    {
+        "name": "Gregg Newman",
+        "photo": "fmkphotos/fmk8.jpg"
+    },
+    {
+        "name": "Patricia Rhodes",
+        "photo": "fmkphotos/fmk9.jpg"
+    },
+    {
+        "name": "Wilbert Sims",
+        "photo": "fmkphotos/fmk10.jpg"
+    }
+]
+`
